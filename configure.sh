65e05e7a107bb65a222ba527ad6dac4db3fd349b
Heros Araujo <herossa@gmail.com>
2016-12-11 23:14:20 +0000

    configure.sh

--- configure.sh
+++ configure.sh
@@ -1,1 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo pip install virtualenv
+virtualenv --python=python3 venv
+venv/bin/python3 -m pip install -r requirements.txt
